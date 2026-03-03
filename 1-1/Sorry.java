7d28583bb4215c049b46ea1f61e7d6768f65c75a
LeeDoyun <134493573+CodingKingDoyun@users.noreply.github.com>
2026-03-03 21:51:21 +0900

    Create Sorry.java

---
+++ 1-1/Sorry.java
@@ -0,0 +1,8 @@
+public class Sorry
+{
+  public static void main(String[] arg){
+    //add code here
+    System.out.println("Sorry~~");
+    System.out.println("I'm sorry that it is so fun~~");
+  }
+}
